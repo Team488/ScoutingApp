fdf9383270f01dcd4e99c93bf4a75b8ea8281c7d
Michael Giambalvo <heathkit@gmail.com>
2020-02-15 14:46:16 -0800

    Upgrade and unlink dependencies to prepare for 0.60.0

--- android/app/src/main/java/com/scoutingapp/MainApplication.java
+++ android/app/src/main/java/com/scoutingapp/MainApplication.java
@@ -3,9 +3,6 @@ package com.scoutingapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.rnfs.RNFSPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new SvgPackage(),
-            new RNGestureHandlerPackage(),
-            new RNFSPackage()
+          new MainReactPackage()
       );
     }
 
